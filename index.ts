194575e42557342b8df3c46faa5a981393445156
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2025-04-22 19:07:11 -0400

    New Env Strategy with GHA

--- index.ts
+++ index.ts
@@ -10,8 +10,6 @@ import QueueHandler from "./src/queue/queue_handler.ts";
 import Settings from "./src/settings/settings.ts";
 import Game from "./src/models/game.ts";
 
-console.log(getEnv("TRN_HOST"))
-
 Database.connect().then(() => {
     const client = new Client(BOT_OPTIONS)
     client.on(Events.ClientReady, ready);
