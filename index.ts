9516c4221d44f77d9d32ec1cc5671b2995a7fe2f
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2025-04-22 21:30:38 -0400

    Test cancel

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import {Client, EmbedBuilder, Events, type Interaction, TextChannel} from "discord.js";
-import {BOT_OPTIONS, ephemeralReply, getEnv} from "./src/utils/utils.ts";
+import {BOT_OPTIONS, ephemeralReply, getEnv, noReply} from "./src/utils/utils.ts";
 import {handleGameAction} from "./src/utils/game.ts";
 import {handleQueueAction} from "./src/utils/queue.ts";
 import util from "node:util";
@@ -86,6 +86,7 @@ async function interactionCreate(interaction: Interaction) {
 
             switch (args[0]) {
                 case "cancel": {
+                    await noReply(interaction);
                     try {
                         await interaction.message.delete();
                     } catch {}
