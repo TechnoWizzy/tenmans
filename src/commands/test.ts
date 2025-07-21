822a808425d9aecfb83d9102e62f954450109e2c
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2025-07-20 22:08:55 -0400

    Test analytics 4

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -46,7 +46,7 @@ async function execute(interaction: ChatInputCommandInteraction, _: Guild) {
                 .map(stuff => {
                     const player = stuff.player
                     const index = sorted.indexOf(stuff);
-                    const rateV = Math.floor(1000 * stuff.cancelRate) * 1000;
+                    const rateV = Math.floor(1000 * stuff.cancelRate) / 1000;
                     const rate = `${String(100 * rateV)}% games cancelled`
                     return `**${index + 1} ${player.username}** - ${rate}`;
                 })
