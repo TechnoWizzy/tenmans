f4022bc40c16a2f00297dfa562d098dac49d9e45
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2025-08-09 17:32:37 -0400

    work on stats command

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -1,6 +1,6 @@
 import {ChatInputCommandInteraction, EmbedBuilder, type Guild, SlashCommandBuilder} from "discord.js";
 import {Command} from "./command.ts";
-import {ephemeralReply} from "../utils/utils.ts";
+import {ephemeralReply, getEnv} from "../utils/utils.ts";
 import {Player} from "../models/player.ts";
 import {Tracker} from "../utils/tracker.ts";
 
@@ -35,11 +35,16 @@ async function execute(interaction: ChatInputCommandInteraction, _: Guild) {
 }
 
 function createProfileEmbed(data: ProfileData) {
-    const embed = new EmbedBuilder();
+    const embed = new EmbedBuilder()
+        .setTitle(data.platformInfo.platformUserHandle)
+        .setURL(getEnv("TRACKER_URL_PROFILE") + encodeURIComponent(data.platformInfo.platformUserHandle))
+        .setImage(data.platformInfo.avatarUrl)
+
     for (const segment of data.segments) {
         switch (segment.type) {
             case "peak-rating":
-
+                const ratingStats = segment.stats;
+                embed.setThumbnail(ratingStats.peakRating.metadata.iconUrl);
                 break;
 
             case "season":
